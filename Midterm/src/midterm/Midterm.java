de788effd7666dd0d16f0a31e08b50a950f820d2
Dan Elijah Inocentes <inocentes.danelijah@gmail.com>
2016-09-17 14:22:45 +0800

    Adds file reader

--- Midterm/src/midterm/Midterm.java
+++ Midterm/src/midterm/Midterm.java
@@ -1,10 +1,16 @@
 package midterm;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.io.FileReader;
 
 public class Midterm {
 
     
-    public static void main(String[] args) {
-       
+    public static void main(String[] args) throws FileNotFoundException {
+       Scanner in = new Scanner (new FileReader("input.txt"));
+       while(in.hasNext()){
+           System.out.println(in.nextLine());
+       }
     }
     
 }
